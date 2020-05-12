0fc8e9e6562f716d27ef59147ad650849a25dfa6
alexjay1997 <aljayfauni@gmail.com>
2020-05-13 00:04:59 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagedropz - Webpage-Builder</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet'>
+    
+</head>
+<body>
+     <div class="page-container">
+        <div class="top-header">
+            <div class="container">
+                <div class="main-menu">
+                    <div class="logo-wrapper">
+                      <img src="assets/img/logo.png"><label>Pagedropz</label>
+                    </div>
+                    <div class="nav">
+                        <ul>
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#">FEATURES</a></li>
+                            <li><a href="#">DEMO</a></li>
+                            <li style="border:1px solid #eee ;" class="login"><a href="#">Login</a></li>
+                      
+                        </ul>
+                    </div>    
+                </div>
+            </div>
+        
+     </div> 
+     <section class="kv">
+        <div class="kv-content">
+            <div class="container">
+                <div class="kv-text-left">
+                <label  style="line-height:50px;font-family:Lobster;font-size:46px;">Making Your Website Should be easy</label><br><br>
+                <label  style="line-height:50px;font-family:Lobster,sans-serif;font-size:42px;">Create your Website Now </label>
+                <br> <br>
+                <label  style="font-size:24px;font-family:calibri;">With Pagedropz Webpage Builder 2.0</label>
+                <br><br>
+                <button class="btn-start">Start</button>
+
+            </div>
+        </div>
+        <br><br>
+     </section> 
+     <section class="features">
+     </section>
+</div>
+</body>
+</html>
